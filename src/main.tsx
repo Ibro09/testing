e7f89cd25b1caf826f1df1888c1196285ee2a249
ibrahim abdulsalam <ibsalam24@gmail.com>
2024-10-16 16:13:28 +0100

    second commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import Friend from './Routes/Friends.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Friend/>
+    <App/>
   </React.StrictMode>,
 )
